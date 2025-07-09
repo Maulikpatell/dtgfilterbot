6df140b9be69ee39f212cc20d3bf908a688bbdeb
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2025-07-09 23:24:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,7 +13,6 @@ from typing import List
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
-import shortzy
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
