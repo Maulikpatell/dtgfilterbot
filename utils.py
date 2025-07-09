a84c9daae0e2c7daf7eaa12fed4299e41cb15420
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2025-07-09 23:23:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -345,12 +345,3 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-shortz = shortzy.Shortzy(SHORTENER_API, "dtglinks.in")
-async def get_shortlink(link):
-    if SHORTENER_API:
-        if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
-            return await shortz.get_quick_link(link)
-        else:
-            return await shortz.convert(link, silently_fail=False)
-    return link
